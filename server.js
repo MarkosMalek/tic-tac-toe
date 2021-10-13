c78042b6271be5f1faa5ae9c69a316968c7cfb98
MarkousMalek <MarkousMalek@gmail.com>
2021-10-13 05:13:58 +0200

    server port

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const PORT = 8080 || process.env.PORT;
 
 app.use(express.json());
 /* Dependencies */
@@ -17,4 +16,4 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static("client"));
 
-app.listen(PORT, () => console.log(`server on Port ${PORT}`));
+app.listen(process.env.PORT || 8080);
